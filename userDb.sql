11c7533111c26134f8732ed0b69920111306cbf4
Nolan Newman <123785135+nolannewman@users.noreply.github.com>
2024-04-07 18:01:17 -0700

    Update Database Schema
    Updated database schema to be compliant with the live version on AWS.

--- userDb.sql
+++ userDb.sql
@@ -11,13 +11,31 @@ CREATE TABLE mmUser(
 );
 
 CREATE TABLE blogPost(
-  post_id INT PRIMARY KEY NOT NULL,
+  post_id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   content VARCHAR(255) NOT NULL,
   poster VARCHAR(32) NOT NULL
+);
+
+CREATE TABLE foodLog(
+	food_id INT AUTO_INCREMENT PRIMARY KEY,
+	food_name VARCHAR(32),
+    food_desc VARCHAR(127),
+    calories_eaten INT,
+	day_eaten date,
+	username VARCHAR(32)
+);
+
+CREATE TABLE exerciseLog(
+	num_steps INT,
+	calories_burnt INT,
+    day_logged date,
+	username VARCHAR(32)
 );
 
 SHOW tables;
 
 SELECT * FROM mmUser;
 SELECT * FROM blogPost;
+SELECT * FROM foodLog;
+SELECT * FROM exerciseLog;
